58aaa5784eb3a3170904032b8f8407f0fe47029c
Mark Wutka <mark.wutka@banyanhills.com>
2016-01-27 09:14:57 -0600

    Changed digitrec.hs to use minimumBy, added comments and command-line args

--- mark_wutka+haskell/digitrec.hs
+++ mark_wutka+haskell/digitrec.hs
@@ -1,40 +1,62 @@
+import System.Environment (getArgs)
 import Data.List
+import Data.Ord (compare)
+
+-- Digit Recognizer written during NashFP meeting on 1-26-2016
+-- Compile with: ghc -O2 digitrec.hs
+-- Run with: digitrec testsamplefile trainingsamplefile
+-- Example run: digitrec ../test-sample.csv ../training-sample.csv
 
 replaceComma ',' = ' '
 replaceComma c = c
 
+-- Parses a comma-separated string of numbers into a list of ints
+-- Haskell words function only splits on space so this function changes
+-- commas to spaces before calling words. Then it maps the read function
+-- over the list of strings returned by words.
 parseLine :: String -> [Int]
 parseLine s =
     map read $ words (map replaceComma s)
 
+-- Reads the file content, uses lines to split it into a list of strings,
+-- then maps parseLine over the list of lines
 loadData filename = do
     filedata <- readFile filename
     return (map parseLine (tail (lines filedata)))
 
-compareSamples :: [Int] -> [Int] -> Int    
+-- Compares two lists of pixels by summing the squares of the differences
+-- Returns a pair containing the test sample digit and its score
+compareSamples :: [Int] -> [Int] -> (Int,Int)
 compareSamples trainingSample testSample =
-  sum $ zipWith comparePixels (tail testSample) (tail trainingSample)
+  (head testSample,
+    sum $ zipWith comparePixels (tail testSample) (tail trainingSample))
       where
         comparePixels p1 p2 = sqr (p1 - p2)
         sqr x = x * x
 
+-- Finds the closes match by looking for the one with the minimum score
 recognizeSample :: [Int] -> [[Int]] -> (Int,Int)
 recognizeSample trainingSample testSamples =
-  foldl' scoreSample (-1,9999999999) testSamples
-      where
-        scoreSample (bestDigit,bestScore) sample = if newScore sample < bestScore then (head sample, newScore sample) else (bestDigit,bestScore)
-        newScore sample = compareSamples trainingSample sample
+  minimumBy compareScores (map (compareSamples trainingSample) testSamples)
+    where
+      compareScores (d1,s1) (d2,s2) = compare s1 s2 
 
+-- Returns true if the recognizer recognized the correct digit
 sampleMatches :: [[Int]] -> [Int] -> Bool
 sampleMatches testSamples trainingSample =
     head trainingSample == fst (recognizeSample trainingSample testSamples)
 
 main = do
-  testSample <- loadData "../test-sample.csv"
-  trainingSample <- loadData "../training-sample.csv"
---  let result = recognizeSample trainingSample testSample
+  -- Get the command line argumentsw
+  argv <- getArgs
+  let [testfile,trainingfile] = take 2 argv
+
+  -- Load the data
+  testSample <- loadData testfile
+  trainingSample <- loadData trainingfile
+
+  -- Count the number of matches
   let numMatched = length $ filter (sampleMatches trainingSample) testSample
+
   let successRate = (fromIntegral numMatched) / (fromIntegral $ length testSample)
-  putStrLn $ show numMatched
-  putStrLn $ show (length testSample)
   putStrLn $ show successRate
